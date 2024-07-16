57657336da509ec4a1bae352b5d66bad10a0e167
yogi-tud <johannes.fett@tu-dresden.de>
2024-07-16 06:26:45 +0000

    y

--- run_omp_threads.sh
+++ run_omp_threads.sh
@@ -1,14 +1,13 @@
-./gpuusm -m 16 -omp 1 -o omp_cpu_sycl.csv -d cpu
-./gpuusm -m 16 -omp 2 -o omp_cpu_sycl.csv -d cpu
-./gpuusm -m 16 -omp 4 -o omp_cpu_sycl.csv -d cpu
-./gpuusm -m 16 -omp 8 -o omp_cpu_sycl.csv -d cpu
-./gpuusm -m 16 -omp 16 -o omp_cpu_sycl.csv -d cpu
-./gpuusm -m 512 -omp 1 -o omp_cpu_sycl.csv -d cpu
-./gpuusm -m 512 -omp 2 -o omp_cpu_sycl.csv -d cpu
-./gpuusm -m 512 -omp 4 -o omp_cpu_sycl.csv -d cpu
-./gpuusm -m 512 -omp 8 -o omp_cpu_sycl.csv -d cpu
-./gpuusm -m 512 -omp 16 -o omp_cpu_sycl.csv -d cpu
-
+./gpuusm -m 1024 -omp 1 -o omp_cpu_sycl.csv -d cpu
+./gpuusm -m 1024 -omp 2 -o omp_cpu_sycl.csv -d cpu
+./gpuusm -m 1024 -omp 4 -o omp_cpu_sycl.csv -d cpu
+./gpuusm -m 1024 -omp 8 -o omp_cpu_sycl.csv -d cpu
+./gpuusm -m 1024 -omp 16 -o omp_cpu_sycl.csv -d cpu
+./gpuusm -m 2048 -omp 1 -o omp_cpu_sycl.csv -d cpu
+./gpuusm -m 2048 -omp 2 -o omp_cpu_sycl.csv -d cpu
+./gpuusm -m 2048 -omp 4 -o omp_cpu_sycl.csv -d cpu
+./gpuusm -m 2048 -omp 8 -o omp_cpu_sycl.csv -d cpu
+./gpuusm -m 2048 -omp 16 -o omp_cpu_sycl.csv -d cpu
 
 
 
