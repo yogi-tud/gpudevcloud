5e827ec1f75e4e3cff3e6a867bee64fa62f41906
yogi-tud <johannes.fett@tu-dresden.de>
2024-07-17 05:59:34 +0000

    sizes

--- run.sh
+++ run.sh
@@ -1,11 +1,39 @@
-./gpuusm -k 1 -o min.csv -d cpu -omp 4 -s 1
-./gpuusm -k 1 -o min.csv -d gpu -omp 4 -s 1
-./gpuusm -k 1 -o min.csv -d cpu -omp 4 -s 1
-./gpuusm -k 1 -o min.csv -d gpu -omp 4 -s 1
-./gpuusm -k 1 -o min.csv -d cpu -omp 4 -s 0
-./gpuusm -k 1 -o min.csv -d gpu -omp 4 -s 0
-./gpuusm -k 1 -o min.csv -d cpu -omp 4 -s 0
-./gpuusm -k 1 -o min.csv -d gpu -omp 4 -s 0
+./gpuusm -k 1 -o sizes.csv -d gpu -omp 4 -s 0
+./gpuusm -k 1 -o sizes.csv -d gpu -omp 4 -s 0
+./gpuusm -k 4 -o sizes.csv -d gpu -omp 4 -s 0
+./gpuusm -k 16 -o sizes.csv -d gpu -omp 4 -s 0
+./gpuusm -k 64 -o sizes.csv -d gpu -omp 4 -s 0
+./gpuusm -k 128 -o sizes.csv -d gpu -omp 4 -s 0
+./gpuusm -k 512 -o sizes.csv -d gpu -omp 4 -s 0
+./gpuusm -m 1 -o sizes.csv -d gpu -omp 4 -s 0
+./gpuusm -m 1 -o sizes.csv -d gpu -omp 4 -s 0
+./gpuusm -m 4 -o sizes.csv -d gpu -omp 4 -s 0
+./gpuusm -m 16 -o sizes.csv -d gpu -omp 4 -s 0
+./gpuusm -m 64 -o sizes.csv -d gpu -omp 4 -s 0
+./gpuusm -m 128 -o sizes.csv -d gpu -omp 4 -s 0
+./gpuusm -m 512 -o sizes.csv -d gpu -omp 4 -s 0
+./gpuusm -m 1024 -o sizes.csv -d gpu -omp 4 -s 0
+./gpuusm -m 2048 -o sizes.csv -d gpu -omp 4 -s 0
+./gpuusm -m 4096 -o sizes.csv -d gpu -omp 4 -s 0
+./gpuusm -k 1 -o sizes.csv -d cpu -omp 4 -s 0
+./gpuusm -k 1 -o sizes.csv -d cpu -omp 4 -s 0
+./gpuusm -k 4 -o sizes.csv -d cpu -omp 4 -s 0
+./gpuusm -k 16 -o sizes.csv -d cpu -omp 4 -s 0
+./gpuusm -k 64 -o sizes.csv -d cpu -omp 4 -s 0
+./gpuusm -k 128 -o sizes.csv -d cpu -omp 4 -s 0
+./gpuusm -k 512 -o sizes.csv -d cpu -omp 4 -s 0
+./gpuusm -m 1 -o sizes.csv -d cpu -omp 4 -s 0
+./gpuusm -m 1 -o sizes.csv -d cpu -omp 4 -s 0
+./gpuusm -m 4 -o sizes.csv -d cpu -omp 4 -s 0
+./gpuusm -m 16 -o sizes.csv -d cpu -omp 4 -s 0
+./gpuusm -m 64 -o sizes.csv -d cpu -omp 4 -s 0
+./gpuusm -m 128 -o sizes.csv -d cpu -omp 4 -s 0
+./gpuusm -m 512 -o sizes.csv -d cpu -omp 4 -s 0
+./gpuusm -m 1024 -o sizes.csv -d cpu -omp 4 -s 0
+./gpuusm -m 2048 -o sizes.csv -d cpu -omp 4 -s 0
+./gpuusm -m 4096 -o sizes.csv -d cpu -omp 4 -s 0
+
+
 
 
 
